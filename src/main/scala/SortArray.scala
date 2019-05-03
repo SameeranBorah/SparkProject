502b840cfca2f55f4b9d946534e4fc641996b08a
Sameeran Borah <sameeranborah@gmail.com>
2019-05-03 17:45:50 +0530

    changes made to input array

--- src/main/scala/SortArray.scala
+++ src/main/scala/SortArray.scala
@@ -7,8 +7,10 @@ object SortArray {
       .setAppName("Sort Array")
       .setSparkHome("src/main/resources")
     val sc = new SparkContext(conf)
-    val rdd1 = sc.parallelize(Seq(("India",91),("USA",1),("Brazil",55),("Greece",30),("China",86),("Sweden",46),("Turkey",90),("Nepal",977)))
-    val rdd2 = rdd1.sortByKey()
-      rdd2.collect().foreach(println);
+    val collection = Array(3, 6, 5, 9, 2, 8, 1, 4, 10)
+    val rdd = sc.parallelize(collection)
+    //val rdd1 = sc.parallelize(Seq(("India",91),("USA",1),("Brazil",55),("Greece",30),("China",86),("Sweden",46),("Turkey",90),("Nepal",977)))
+    val rdd1 = rdd.map(x => (x, x)).sortByKey(false).map(_._1)
+    rdd1.collect().foreach(println);
   }
 }
